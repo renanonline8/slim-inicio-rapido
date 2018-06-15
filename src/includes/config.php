17b7bf9f44b2197afadf06ae05fade97a8f2088b
Renan Santos Gomes <renanonline8@gmail.com>
2018-06-15 16:59:18 +0000

    Ajuste para compatibilidade com Linux

--- src/includes/config.php
+++ src/includes/config.php
@@ -3,7 +3,7 @@
 $config['displayErrorDetails'] = true;
 
 \ActiveRecord\Config::initialize(function($cfg) {
-    $cfg->set_model_directory(__DIR__ . '/../app/models');
+    $cfg->set_model_directory(__DIR__ . '/../App/Models');
     $cfg->set_connections(
         array(
             'development' => 'mysql://inicioSlim:ad23ol12@localhost/slim_inicio_rapido'
