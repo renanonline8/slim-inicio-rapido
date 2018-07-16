c77581dbb573d24acfd65471a4eb9e51f14f6f0a
Renan Santos Gomes <renanonline8@gmail.com>
2018-07-16 20:51:40 -0300

    Adicionado dados do usuário para a página de dados

--- src/App/Controller/ControllerUsuario.php
+++ src/App/Controller/ControllerUsuario.php
@@ -4,11 +4,18 @@ namespace App\Controller;
 use Utils\Controller\Controller;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use App\Models\Usuario;
 
 final class ControllerUsuario extends Controller
 {
     public function home(Request $request, Response $response, Array $args)
     {
+        $dadosSessao = $this->twigArgs->retArgs('sessao');
+        $usuario = Usuario::find_by_id_externo($dadosSessao['id']);
+        $dadosUsuario = $usuario->to_array(array(
+            'except' => 'senha'
+        ));
+        $this->twigArgs->adcDados('usuario', $dadosUsuario);
         return $this->view->render($response, 'usuario-dados.twig', $this->twigArgs->retArgs());
     }
     
